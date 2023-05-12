2368a52336f92512142c24b370cf513b3f76deb2
Mikhail Bychkov <mikbychkov@gmail.com>
2023-05-13 02:12:03 +0700

    Add MainInstaller for GUI

---
+++ Assets/Project/GUI/Code/Scripts/CRL/Installers/MainInstaller.cs
@@ -0,0 +1,29 @@
+﻿using TicTacToe.GUI.Configs;
+using TicTacToe.GUI.Systems.ModalWindowManagement;
+using UnityEngine;
+using Zenject;
+
+namespace TicTacToe.GUI.CRL.Installers
+{
+    public class MainInstaller : MonoInstaller
+    {
+        [SerializeField] private MainConfig _mainConfig;
+        [SerializeField] private ModalWindowManager _modalWindowManager;
+        [SerializeField] private ModalWindow _modalWindowPrefab;
+
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<MainConfig>()
+                .FromInstance(_mainConfig);
+
+            Container
+                .Bind<ModalWindowManager>()
+                .FromInstance(_modalWindowManager);
+
+            Container
+                .BindFactory<ModalWindow, ModalWindowFactory>()
+                .FromComponentInNewPrefab(_modalWindowPrefab);
+        }
+    }
+}
