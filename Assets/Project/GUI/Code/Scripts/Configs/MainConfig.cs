ad395619b3b0e24f6665dedae28a73e07b01cbc7
Mikhail Bychkov <mikbychkov@gmail.com>
2023-05-13 02:02:09 +0700

    Add GUI MainConfig

---
+++ Assets/Project/GUI/Code/Scripts/Configs/MainConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TicTacToe.GUI.Configs
+{
+    [CreateAssetMenu(fileName = "MainConfig", menuName = "Config/UI/MainConfig")]
+    internal class MainConfig : ScriptableObject
+    {
+        [SerializeField] private Color _playerColorX;
+        [SerializeField] private Color _playerColorO;
+
+        public Color PlayerColorX => _playerColorX;
+        public Color PlayerColorO => _playerColorO;
+    }
+}
