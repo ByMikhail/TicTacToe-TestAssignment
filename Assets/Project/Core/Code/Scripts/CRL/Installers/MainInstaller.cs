d5a8432f9ae2d20b3bd6437a3a597e00c419f7f2
Mikhail Bychkov <mikbychkov@gmail.com>
2023-05-13 01:29:13 +0700

    Add MainInstaller for Core

---
+++ Assets/Project/Core/Code/Scripts/CRL/Installers/MainInstaller.cs
@@ -0,0 +1,25 @@
+﻿using TicTacToe.Core.GameModel;
+using TicTacToe.Core.Infrastructure.SceneManagement;
+using Zenject;
+
+namespace TicTacToe.Core.CRL.Installers
+{
+    public class MainInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<TicTacToeGame>()
+                .AsSingle()
+                .NonLazy();
+
+            Container
+                .Bind<SceneManager>()
+                .AsSingle();
+
+            Container
+                .Bind<GameController>()
+                .AsSingle();
+        }
+    }
+}
