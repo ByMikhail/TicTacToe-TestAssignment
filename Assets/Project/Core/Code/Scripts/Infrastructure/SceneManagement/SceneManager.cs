969dc030d882686817f1d1e2e0c45a4de107b4ca
Mikhail Bychkov <mikbychkov@gmail.com>
2023-05-13 01:27:23 +0700

    Add SceneManager

---
+++ Assets/Project/Core/Code/Scripts/Infrastructure/SceneManagement/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace TicTacToe.Core.Infrastructure.SceneManagement
+{
+    public class SceneManager
+    {
+        public void ReloadCurrentScene()
+        {
+            var currentScene = UnitySceneManager.GetActiveScene();
+            UnitySceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
