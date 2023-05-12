c198d2439f818def5b737f6680552fadd549f335
Mikhail Bychkov <mikbychkov@gmail.com>
2023-05-13 01:27:55 +0700

    Add GameController

---
+++ Assets/Project/Core/Code/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using TicTacToe.Core.Infrastructure.SceneManagement;
+using Zenject;
+
+namespace TicTacToe.Core
+{
+    public class GameController
+    {
+        private SceneManager _sceneManager;
+
+        [Inject]
+        private void InjectDependencies(SceneManager sceneManager)
+        {
+            _sceneManager = sceneManager;
+        }
+
+        public void StartNewGame()
+        {
+            _sceneManager.ReloadCurrentScene();
+        }
+    }
+}
